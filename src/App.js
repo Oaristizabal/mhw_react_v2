55489b1d1c8e24f4a12e4e899fbe080c596f2546
oaristizabal <oaristizabal@kaufmanrossin.com>
2021-08-06 11:26:35 -0400

    adding input boxes and additional libraries

--- src/App.js
+++ src/App.js
@@ -1,19 +1,80 @@
+import React, { useState } from "react"
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Grid,
+  Typography,
+  TextField
+} from "@material-ui/core";
+
+// put this in a css file?
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
 
 
 function App() {
+  const classes = useStyles();
+
+  //initial states 
+  const [selectedDate, setSelectedDate] = useState(new Date('2014-08-18'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
-    <div className="App">
+    <div>
+      <Card>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="From" variant="outlined" />
+          <TextField id="outlined-basic" label="To" variant="outlined" />
 
-      <h1>Hello</h1>
-
-
-
-
-
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="MM/dd/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Date picker inline"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Time picker"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change time',
+              }}
+            />
+          </MuiPickersUtilsProvider>
+        </form>
+      
+      </Card>
+     
     </div>
+
   );
 }
 
